b15b3026b8fda8d7fd02d96944e4047419ce0211
Ellora Virtue <ellora.virtue@hotmail.com>
2024-10-12 20:01:17 +0200

    add mock data

---
+++ data/gameData.ts
@@ -0,0 +1,24 @@
+import { GameData } from '../@types';
+
+export const GAME_DATA: GameData = [
+  {
+    category: 'Jumping animals',
+    words: ['Cricket', 'Frog', 'Hare', 'Kangaroo'],
+    difficulty: 1,
+  },
+  {
+    category: 'Apply pressure to',
+    words: ['Crush', 'Mash', 'Press', 'Squash'],
+    difficulty: 2,
+  },
+  {
+    category: 'Olympic sports',
+    words: ['Breaking', 'Hockey', 'Skeleton', 'Trampoline'],
+    difficulty: 3,
+  },
+  {
+    category: 'Things you can set',
+    words: ['Mood', 'Record', 'Table', 'Volleyball'],
+    difficulty: 4,
+  },
+];
