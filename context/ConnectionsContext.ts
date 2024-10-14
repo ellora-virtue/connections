c574ce25e36747d07194355b7c47c9bb4eb199ad
Ellora Virtue <ellora.virtue@hotmail.com>
2024-10-14 18:42:37 +0200

    animate tile shuffle with fade out/in

--- context/ConnectionsContext.ts
+++ context/ConnectionsContext.ts
@@ -4,6 +4,7 @@ import { useRequiredContext } from '../hooks';
 import { GAME_DATA } from '../data/gameData';
 import { Tile } from '../@types';
 import shuffle from 'lodash/shuffle';
+import { runOnJS, SharedValue, useSharedValue, withTiming } from 'react-native-reanimated';
 
 const TOTAL_HORIZONTAL_PADDING = 16;
 const TOTAL_TILES_PADDING = 24;
@@ -11,6 +12,7 @@ const NUMBER_OF_TILES = 4;
 
 export type ConnectionsContextValue = {
   tileWidth: number;
+  tileTextOpacity: SharedValue<number>;
 
   unguessedTiles: Tile[];
 
@@ -31,6 +33,7 @@ export const useConnectionsContext = () => useRequiredContext(ConnectionsContext
 export const useProvideConnectionsState = (): ConnectionsContextValue => {
   const { width: screenWidth } = Dimensions.get('screen');
   const tileWidth = (screenWidth - TOTAL_HORIZONTAL_PADDING - TOTAL_TILES_PADDING) / NUMBER_OF_TILES;
+  const tileTextOpacity = useSharedValue(1);
 
   const [unguessedTiles, setUnguessedTiles] = useState<Tile[]>(
     shuffle(
@@ -73,10 +76,24 @@ export const useProvideConnectionsState = (): ConnectionsContextValue => {
 
   const handleDeselectAll = () => setSelectedTiles(new Set());
 
-  const shuffleUnguessedTiles = () => setUnguessedTiles((prev) => shuffle(prev));
+  const shuffleUnguessedTiles = () => {
+    // Fade out first
+    tileTextOpacity.value = withTiming(0, { duration: 150 }, (isFinished) => {
+      if (isFinished) {
+        // Shuffle and fade back in
+        runOnJS(shuffleTiles)();
+      }
+    });
+  };
+
+  const shuffleTiles = () => {
+    setUnguessedTiles((prev) => shuffle(prev));
+    tileTextOpacity.value = withTiming(1, { duration: 150 });
+  };
 
   return {
     tileWidth,
+    tileTextOpacity,
     unguessedTiles,
     selectedTiles,
     handleTilePress,
