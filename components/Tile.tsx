87764c4f5ad8af392ee3524316836b9795ee243d
Ellora Virtue <ellora.virtue@hotmail.com>
2024-10-15 19:02:28 +0200

    add animation for tile press

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pressable } from 'react-native';
-import Animated, { useAnimatedStyle } from 'react-native-reanimated';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import tw from 'twrnc';
 import { Tile as TileType } from '../@types';
 import { COLORS } from '../constants';
@@ -14,31 +14,57 @@ type TileProps = {
 const TILE_HEIGHT = 84;
 const MAX_TILE_WIDTH = 112;
 
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
+
 export const Tile = ({ word }: TileProps) => {
-  const { tileWidth, tileTextOpacity, selectedTiles, handleTilePress } = useConnectionsContext();
+  const { tileWidth, tileTextOpacity, selectedTiles, onTilePress } = useConnectionsContext();
   const fontSize = useResponsiveFontSize({ word, tileWidth });
 
-  const isSelected = selectedTiles.has(word);
+  const tileScale = useSharedValue(1);
 
-  const onTilePress = () => handleTilePress(word);
+  const animatedTileStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: tileScale.value }],
+  }));
 
-  const animatedStyle = useAnimatedStyle(() => ({
+  const animatedTextStyle = useAnimatedStyle(() => ({
     opacity: tileTextOpacity.value,
   }));
 
+  const isSelected = selectedTiles.has(word);
+
+  const handleTilePress = () => onTilePress(word);
+
+  const handleLongPress = () => {
+    onTilePress(word);
+
+    // Scale down to 90% on long press (75ms)
+    tileScale.value = withTiming(0.9);
+  };
+
+  const handlePressOut = () => {
+    tileScale.value = withTiming(1); // Scale back to original when released
+  };
+
   return (
-    <Pressable
-      style={tw`py-4 ${isSelected ? `bg-[${COLORS.surface.dark}]` : `bg-[${COLORS.surface.light}]`} items-center justify-center rounded-lg px-2 w-[${tileWidth}px] max-w-[${MAX_TILE_WIDTH}px] h-[${TILE_HEIGHT}px]`}
-      onPress={onTilePress}
+    <AnimatedPressable
+      style={[
+        animatedTileStyle,
+        tw`py-4 ${isSelected ? `bg-[${COLORS.surface.dark}]` : `bg-[${COLORS.surface.light}]`} items-center justify-center rounded-lg px-2 w-[${tileWidth}px] max-w-[${MAX_TILE_WIDTH}px] h-[${TILE_HEIGHT}px]`,
+      ]}
+      onPress={handleTilePress}
+      onLongPress={handleLongPress}
+      // Overrides long press default threshold of 300ms
+      delayLongPress={75}
+      onPressOut={handlePressOut}
     >
       <Animated.Text
         style={[
-          animatedStyle,
+          animatedTextStyle,
           tw`text-center font-bold uppercase text-[${fontSize}px] tracking-tight ${isSelected ? 'text-white' : 'text-black'}`,
         ]}
       >
         {word}
       </Animated.Text>
-    </Pressable>
+    </AnimatedPressable>
   );
 };
