d1f86c57f653de2f2266e526d2bc28a2a0decb42
Ellora Virtue <ellora.virtue@hotmail.com>
2024-10-15 17:38:08 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 module.exports = {
   content: [
     './App.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}', // Adjust this path according to your folder structure
-    './components/**/*.{js,jsx,ts,tsx}', // Adjust this path according to your folder structure
+    './components/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
