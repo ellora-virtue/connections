cbdfb19c1b23872d563403898043af52dd9f82fa
Ellora Virtue <ellora.virtue@hotmail.com>
2024-10-15 19:08:39 +0200

    tile color animation - wip

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pressable } from 'react-native';
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Animated, { interpolateColor, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import tw from 'twrnc';
 import { Tile as TileType } from '../@types';
 import { COLORS } from '../constants';
@@ -21,9 +21,11 @@ export const Tile = ({ word }: TileProps) => {
   const fontSize = useResponsiveFontSize({ word, tileWidth });
 
   const tileScale = useSharedValue(1);
+  const tileColorProgress = useSharedValue(0);
 
   const animatedTileStyle = useAnimatedStyle(() => ({
     transform: [{ scale: tileScale.value }],
+    backgroundColor: interpolateColor(tileColorProgress.value, [0, 1], [COLORS.surface.light, COLORS.surface.dark]),
   }));
 
   const animatedTextStyle = useAnimatedStyle(() => ({
@@ -34,11 +36,17 @@ export const Tile = ({ word }: TileProps) => {
 
   const handleTilePress = () => onTilePress(word);
 
+  // TODO: fix color animation on tile press
+  // Works but first time color doesn't change?
+  // Should I also animate the text color changing?
+
   const handleLongPress = () => {
     onTilePress(word);
 
     // Scale down to 90% on long press (75ms)
     tileScale.value = withTiming(0.9);
+
+    tileColorProgress.value = withTiming(isSelected ? 1 : 0);
   };
 
   const handlePressOut = () => {
@@ -49,7 +57,7 @@ export const Tile = ({ word }: TileProps) => {
     <AnimatedPressable
       style={[
         animatedTileStyle,
-        tw`py-4 ${isSelected ? `bg-[${COLORS.surface.dark}]` : `bg-[${COLORS.surface.light}]`} items-center justify-center rounded-lg px-2 w-[${tileWidth}px] max-w-[${MAX_TILE_WIDTH}px] h-[${TILE_HEIGHT}px]`,
+        tw`items-center justify-center rounded-lg px-2 py-4 w-[${tileWidth}px] max-w-[${MAX_TILE_WIDTH}px] h-[${TILE_HEIGHT}px]`,
       ]}
       onPress={handleTilePress}
       onLongPress={handleLongPress}
